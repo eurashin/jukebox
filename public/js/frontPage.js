6622111b3b347f32c542c0291b45d379e812ac1f
Eura Shin <eesh227@uky.edu>
2018-11-03 22:14:36 -0400

    hi

--- public/js/frontPage.js
+++ public/js/frontPage.js
@@ -1,10 +1,11 @@
 function createJam(userUri) {
-    var website = "https://jukebox-node-8080.herokuapp.com/create";
+//    var website = "https://jukebox-node-8080.herokuapp.com/create";
+    var website = "http://localhost:8080/create";
     $.ajax({
         url: website, 
         type: "post", 
         contentType: 'application/json; charset=utf-8',
-        dataType: 'jsonp',
+        dataType: 'json',
         headers: {
             useruri:userUri,
             'Access-Control-Allow-Credentials':true,
