4ee423247a7b5565fabd435029ad7e17a42a8d7f
Eura Shin <eesh227@uky.edu>
2018-11-04 01:07:17 -0400

    hi

--- public/js/frontPage.js
+++ public/js/frontPage.js
@@ -20,7 +20,29 @@ function createJam(userUri) {
             newDoc.write(data);
             newDoc.close();
     }, 'text');
-    
+}
 
+function joinJam(hostUri, userUri) {
+//    var website = "https://jukebox-node-8080.herokuapp.com/create";
+    var website = "http://localhost:8080/create";
+    $.ajaxSetup({
+        headers: {
+            useruri:userUri,
+            'dataType': "text",
+            'Access-Control-Allow-Credentials':true,
+            'Access-Control-Allow-Origin':true, 
+            'content-type': 'text/html',
+            'Accept': 'text/html',
+        }, 
+        error: function(jqxhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+
+    $.get(website, function(data) {
+            var newDoc = document.open("text/html", "replace");
+            newDoc.write(data);
+            newDoc.close();
+    }, 'text');
 }
 
